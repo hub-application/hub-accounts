01263f9e0cea1761851318fb4e47a8c1d2976e83
Jma353 <jma353@cornell.edu>
2017-02-03 18:11:31 -0500

    Start to boilerplating

--- src/main/java/accounts/models/Session.java
+++ src/main/java/accounts/models/Session.java
@@ -1,8 +1,15 @@
 package accounts.models;
 
+import java.util.Date;
 
+/**
+ * Session used to authenticate users on REST API calls
+ */
 public class Session {
 
-
+  /* Fields */
+  private String sessionToken;
+  private Date expiresAt;
+  private String updateToken;
 
 }
--- src/main/java/accounts/models/User.java
+++ src/main/java/accounts/models/User.java
@@ -1,6 +1,16 @@
 package accounts.models;
 
-
+/**
+ * Hub user (Facebook, Spotify, and SoundCloud credentials)
+ */
 public class User {
+
+  /* Fields */
+  private String handle;
+  private String facebookID;
+  private String spotifyID;
+  private Integer soundcloudID;
+  private Boolean isGuest;
+  private Session session;
 
 }

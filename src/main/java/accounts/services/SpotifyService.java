fc1d431af94b326d45b4c6a37cfd781e2f80fbb5
Jma353 <jma353@cornell.edu>
2017-02-07 11:20:33 -0500

    Start Spotify service

--- src/main/java/accounts/services/SpotifyService.java
+++ src/main/java/accounts/services/SpotifyService.java
@@ -1,7 +1,31 @@
 package accounts.services;
 
+import accounts.utils.Constants;
+import com.wrapper.spotify.Api;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpotifyService {
+
+  @Getter private String clientID;
+  @Getter private String clientSecret;
+  @Getter private String redirectURI;
+  @Getter private Api api;
+
+  /**
+   * Constructor
+   */
+  public SpotifyService() {
+    this.clientID = Constants.HUB_SPOTIFY_CLIENT_ID;
+    this.clientSecret = Constants.HUB_SPOTIFY_SECRET;
+    this.redirectURI = Constants.HUB_SPOTIFY_REDIRECT_URI;
+    this.api = Api.builder()
+      .clientId(this.clientID)
+      .clientSecret(this.clientSecret)
+      .redirectURI(this.redirectURI)
+      .build();
+  }
+
+
 }
--- src/main/java/accounts/utils/Constants.java
+++ src/main/java/accounts/utils/Constants.java
@@ -5,8 +5,19 @@ package accounts.utils;
  * global constants of the service
  */
 public class Constants {
+
+  // Secret encryption key
   public static String SECRET_KEY = System.getenv("SECRET_KEY");
+
+  // SoundCloud
   public static String HUB_SOUNDCLOUD_CLIENT_ID = System.getenv("HUB_SOUNDCLOUD_CLIENT_ID");
   public static String HUB_SOUNDCLOUD_SECRET = System.getenv("HUB_SOUNDCLOUD_SECRET");
   public static String HUB_SOUNDCLOUD_REDIRECT_URI = "hubapp://soundcloudReturnAfterLogin";
+
+  // Spotify
+  public static String HUB_SPOTIFY_CLIENT_ID = System.getenv("HUB_SPOTIFY_CLIENT_ID");
+  public static String HUB_SPOTIFY_SECRET = System.getenv("HUB_SPOTIFY_SECRET");
+  public static String HUB_SPOTIFY_REDIRECT_URI = "hubapp://spotifyReturnAfterLogin";
+
+
 }
